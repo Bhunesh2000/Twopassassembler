661a22ae00388a9d329a92a891ffef21113a7a76
Bhunesh <bhunesh18280@iiitd.ac.in>
2019-10-15 19:48:58 +0530

    pass1 done, added opcodes

--- assem.py
+++ assem.py
@@ -1,25 +1,37 @@
-def pass1(line,optable,symtable,littable,pooltable,LC):
+def pass1(line,optable,symtable,LC):
     words=line.split(" ")
     print("words=  "+ str(words))
-    for i in range(len(words)-1):
-        if words[i] in optable :
-            print( words[i] +" "+ optable[words[i]])
-            # symtable[]
-        elif words[i]=="START":
-            print("START found")
-            LC   = words[i + 1]
-        else :
-            symtable[words[i]]=""
-        print("LC=" + str(LC))
-    print(symtable)
-    return optable,symtable,littable,pooltable,LC
-
+    if (words[0][0]!="/"):
+        for i in range(len(words)):
+            if (len(words[i]) != 0):
+                if (words[i][0] != "/"):
+                    if (words[i][-1] != ":"):
+                        if words[i] in optable:
+                            print(words[i] + " = " + optable[words[i]])
+                        elif words[i] == "START":
+                            print("START found")
+                            LC = words[i + 1]
+                        elif words[i] == "END":
+                            print("End found")
+                            break
+                        elif not words[i].isdigit():
+                            if (words[i] not in symtable):
+                                symtable[words[i]] = ""
+                    else:
+                        if (words[i] not in symtable):
+                            symtable[words[i][:-1]] = LC
+                else:
+                    break
+        LC += 1
+    print("LC=" + str(LC))
+    print("symbol table --> ",symtable)
+    return symtable,LC
 
 def assembler():
     # filein=input("Enter the name of the input assembler language file")
     # fileout=input("Enter the name of the output machine language file")
     # fileop=input("Enter the name of the file containing opcodes")
-    filein="input2.txt"
+    filein="inp3.txt"
     fileout="output.txt"
     fileop="opcodes.txt"
     filer=open(filein,"r")
@@ -27,8 +39,6 @@ def assembler():
     fileo=open(fileop,"r")
     optable={}
     symtable={}
-    littable={}
-    pooltable={}
     LC=0
     opcodelines=fileo.readlines()
     for opline in opcodelines:
@@ -37,9 +47,15 @@ def assembler():
         optable[pairs[1]]=pairs[0]
     print("optable=  "+str(optable))
     file=filer.readlines()
-    print(file)
+    print( "input ",file)
     for lines in file:
-        optable,symtable,littable,pooltable,LC=pass1(lines.rstrip("\n"),optable,symtable,littable,pooltable,LC)
-
+        symtable,LC=pass1(lines.rstrip("\n"),optable,symtable,LC)
+    print("first pass finished")
+    print("symbol table --> ",symtable)
+    for i in symtable:
+        if symtable[i]=="":
+            symtable[i]=LC
+            LC+=1
+    print("symbol table --> ",symtable)
 
 assembler()
