9784b03054281da28d929ed1b7cfaa31b4ec5fd3
Sidd-Dino <siddharth.veerappan@gmail.com>
2019-10-15 22:57:10 +0530

    ERROR HANDLING I

--- assem.py
+++ assem.py
@@ -1,32 +1,52 @@
-def pass1(line,optable,symtable,LC):
-    words = line.split(" ")
-    print("words=  " + str(words))
-    if (words[0][0] != "/"):
-        LC += 1
-        for i in range(len(words)):
-            if (len(words[i])!= 0):
-                if (words[i][0] != "/"):
-                    if (words[i][-1]!= ":"):
-                        if words[i] in optable:
-                            print(words[i] + " = " + optable[words[i]])
-                        elif words[i] == "START":
-                            print("START found")
-                            LC = words[i + 1]
-                        elif words[i] == "END":
-                            print("End found")
-                            break
-                        elif not words[i].isdigit():
-                            if (words[i] not in symtable):
-                                symtable[words[i]] = ""
-                    else:
-                        if (words[i] not in symtable):
-                            symtable[words[i][:-1]] = LC
-                else:
-                    break
+def OPCODE_ARG_CHK(WORDS):
+    ZERO_ARG = ["CLA","STP" ]
+    ONE_ARG = ["LAC","SAC","ADD","SUB","BRZ","BRN","BRP","INP","DSP","MUL","DIV"]
+    if( WORDS[0] in ZERO_ARG ):
+        if( len(WORDS) > 1 ):
+            return 1
+    elif( WORDS[0] in ONE_ARG ):
+        if( len(WORDS) > 2 ):
+            return 1
+        elif( len(WORDS) < 2 ):
+            return -1
+    return 0
 
+def pass1(line,optable,symtable,LC):
+    ERROR_CODE = ""
+    words=line.split(" ")
+    while " " in words :
+        del words[ words.index( " " ) ]
+    while "" in words :
+        del words[ words.index( "" ) ]
+    print("words=  "+ str(words))
+
+    for i in range(len(words)):
+        if (words[i][-1] != ":"):
+            if words[i] in optable:
+                print(words[i] + " = " + optable[words[i]])
+                if( OPCODE_ARG_CHK( words[i:] ) == -1 ):
+                    print( "ERROR[04] > line" , LC  )
+                    return symtable,LC,"04"
+                if( OPCODE_ARG_CHK( words[i:] ) == 1 ):
+                    print( "ERROR[04] > line" , LC  )
+                    return symtable,LC,"05"
+            elif words[i] == "START":
+                print("START found")
+                LC = words[i + 1]
+            elif words[i] == "END":
+                print("End found")
+                break
+            elif not words[i].isdigit():
+                if (words[i] not in symtable):
+                    symtable[words[i]] = ""
+        else:
+            if (words[i] not in symtable):
+                symtable[words[i][:-1]] = LC
+    LC += 1
     print("LC=" + str(LC))
-    print("symbol table --> ", symtable)
-    return symtable,LC
+    print("symbol table --> ",symtable)
+    return symtable,LC,ERROR_CODE
+
 
 def pass2(line,optable,symtable,LC,filew):
     words=line.split(" ")
@@ -72,7 +92,7 @@ def assembler():
     fileout="output.txt"
     fileop="opcodes.txt"
     filer=open(filein,"r")
-    filew=open(fileout,"w")
+    filew=open(fileout,"a")
     fileo=open(fileop,"r")
     optable={}
     symtable={}
@@ -82,21 +102,32 @@ def assembler():
         opline=opline.rstrip("\n")
         pairs=opline.split(" ")
         optable[pairs[1]]=pairs[0]
-    print("optable=  "+str(optable))
+    #print("optable=  "+str(optable))
     file=filer.readlines()
-    print( "input ",file)
+    #print( "input ",file)
+    ERROR_FLAG=""
     for lines in file:
-        symtable,LC=pass1(lines.rstrip("\n"),optable,symtable,LC)
-    print("first pass finished")
-    print("symbol table --> ",symtable)
+        #COMMENT REMOVER
+        if( lines.find( " //" ) != -1 ):
+            lines = lines.replace( lines[lines.find( " //" ):] , "" )
+        if( lines.find( "//" ) != -1 ):
+            lines = lines.replace( lines[lines.find( "//" ):] , "" )
+        #EMPTY LINE CHECKER
+        if( lines != "" ):
+            #PASSING LINE TO PASS1
+            symtable,LC,ERROR_CODE=pass1(lines.rstrip("\n"),optable,symtable,LC)
+        #ERROR RETURNED CHECK
+        if ERROR_CODE != "":
+            return 0
+    #print("first pass finished")
+    #print("symbol table --> ",symtable)
     for i in symtable:
         if symtable[i]=="":
-            LC += 1
             symtable[i]=LC
+            LC+=1
     print("symbol table --> ",symtable)
     print("pass 2 started")
     LC=0
     for lines in file:
         LC=pass2(lines.rstrip("\n"),optable,symtable,LC,filew)
-
 assembler()
